b189915f060c088b985e459f1b6bc7aaa2e072a7
Kirah Cox <kirahanncox@gmail.com>
2025-10-18 19:08:37 -0600

    Add project files.

---
+++ AjaxAPIChoice1/wwwroot/scripts/script.js
@@ -0,0 +1,17 @@
+﻿
+
+$('#fetchBtn').on('click', function () {
+    $.ajax({
+        url: `https://catfact.ninja/fact`,
+        method: 'GET',
+        success: function (response) {
+
+            const fact = `<p>${response.fact}</p>`;
+
+            $('#catFact').html(fact);
+        },
+        error: function () {
+            $('#catFact').html('<p>Failed to load cat fact. Please try again later.</p>');
+        }
+    });
+});
